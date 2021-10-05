9a5dd3de052cc763264aa4adf6b0a4c5f7262e53
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-10-05 17:02:25 +0100

    Adding base project, ready to go

---
+++ src/main/java/org/wicket/ms/bcp/calltree/msbcpcalltreeeventservice/MsBcpCalltreeEventServiceApplication.java
@@ -0,0 +1,13 @@
+package org.wicket.ms.bcp.calltree.msbcpcalltreeeventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBcpCalltreeEventServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBcpCalltreeEventServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/wicket/ms/bcp/calltree/msbcpcalltreeeventservice/MsBcpCalltreeEventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wicket.ms.bcp.calltree.msbcpcalltreeeventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBcpCalltreeEventServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
